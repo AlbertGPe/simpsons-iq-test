c5906fc62f31abb96a977ca4e5037e91ae2bc64e
AlbertGPe <albertgarciapedrosa@gmail.com>
2023-09-20 18:00:10 +0200

    question

--- src/App.js
+++ src/App.js
@@ -1,8 +1,37 @@
+import { useState } from "react";
+import questions from "./components/questions";
 
 function App() {
+
+  const [actualQuestion, setActualQuestion] = useState(0);
+  const [puntuation, setPuntuation] = useState(0);
+  const [isFinished, setIsFinished] = useState(false)
+
+  function handleAnswerSubmit (isCorrect, ev){
+    //add puntuation
+    if (isCorrect) {
+      setPuntuation(puntuation + 1)
+    }
+    //add styles correct or incorret answers
+    ev.target.classList.add(isCorrect ? 'correct' : 'incorrect');
+    //pass next question
+  }
+
   return (
-    <main className="App">
-      Hola
+    <main className="app">
+      <div className="left-side">
+        <div className="question-number">
+          <span>Question {actualQuestion + 1} of {questions.length}</span>
+        </div>
+        <div className="title">
+          {questions[actualQuestion].title}
+        </div>
+      </div>
+      <div className="right-side">
+        {questions[actualQuestion].options.map((answer) => (
+          <button key={answer.answer} onClick={(ev) => handleAnswerSubmit(answer.isCorrect, ev)}> {answer.answer} </button>
+        ))}
+      </div>
     </main>
   );
 }
---
+++ src/components/questions.js
@@ -0,0 +1,94 @@
+const questions = [
+  {
+    title: "2 + 2?",
+    options: [
+      { answer: "5", isCorrect: false},
+      { answer: "3", isCorrect: false},
+      { answer: "4", isCorrect: true},
+      { answer: "43", isCorrect: false}
+    ],
+  },
+  {
+    title: "In which European country would you find the Rijksmuseum?",
+    options: [
+      { answer: "Netherlands", isCorrect: true},
+      { answer: "Spain", isCorrect: false},
+      { answer: "Poland", isCorrect: false},
+      { answer: "Austria", isCorrect: false}
+    ],
+  },
+  {
+    title: "How many human players are there on each side in a polo match?",
+    options: [
+      { answer: "4", isCorrect: true},
+      { answer: "6", isCorrect: false},
+      { answer: "5", isCorrect: false},
+      { answer: "11", isCorrect: false}
+    ],
+  },
+  {
+    title: "What is the capital city of Australia?",
+    options: [
+      { answer: "Sydney", isCorrect: false},
+      { answer: "Canberra", isCorrect: true},
+      { answer: "Ohio", isCorrect: false},
+      { answer: "Australia", isCorrect: false}
+    ],
+  },
+  {
+    title: "What language is spoken in Brazil?",
+    options: [
+      { answer: "Portuguese", isCorrect: true},
+      { answer: "Spanish", isCorrect: false},
+      { answer: "Brazileiro", isCorrect: false},
+      { answer: "French", isCorrect: false}
+    ],
+  },
+  {
+    title: "How many days are there in July?",
+    options: [
+      { answer: "28", isCorrect: false},
+      { answer: "30", isCorrect: false},
+      { answer: "29", isCorrect: false},
+      { answer: "31", isCorrect: true}
+    ],
+  },
+  {
+    title: "What is the tallest mountain in the world?",
+    options: [
+      { answer: "Kangchenjunga", isCorrect: false},
+      { answer: "K2", isCorrect: false},
+      { answer: "Mount Everest", isCorrect: true},
+      { answer: "Manaslu", isCorrect: false}
+    ],
+  },
+  {
+    title: "how many zeros are there in a billion?",
+    options: [
+      { answer: "9", isCorrect: true},
+      { answer: "6", isCorrect: false},
+      { answer: "8", isCorrect: false},
+      { answer: "7", isCorrect: false}
+    ],
+  },
+  {
+    title: "Which planet has the most moons?",
+    options: [
+      { answer: "Jupiter", isCorrect: false},
+      { answer: "Saturn", isCorrect: true},
+      { answer: "Earth", isCorrect: false},
+      { answer: "Neptune", isCorrect: false}
+    ],
+  },
+  {
+    title: "What element is denoted by the chemical symbol Sn in the periodic table?",
+    options: [
+      { answer: "Magnesium", isCorrect: false},
+      { answer: "Arsenic", isCorrect: false},
+      { answer: "Tin", isCorrect: true},
+      { answer: "Silver", isCorrect: false}
+    ],
+  }
+]
+
+export default questions;
